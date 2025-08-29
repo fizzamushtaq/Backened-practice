efbe863490dcf23e3af06bd3b7c40f25f0f39021
Fizza <fizach581@gmail.com>
2025-08-30 03:02:33 +0500

    add app to server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 // import express from "express"
 const app = express()
@@ -6,7 +7,13 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/twitter', (req, res) => {
+  res.send('new world')
+})
+app.get('/login', (req, res) => {
+  res.send('hi lets unlock the backened')
+})
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
